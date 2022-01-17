fce2fe186042d9c6dd388f27c686320074b99ff5
Fabio Rezende de Andrade <rezendefabio@gmail.com>
2022-01-17 12:43:06 -0300

    Conectando com jsoup no google

--- src/main/java/br/com/fra/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/fra/brasileiraoapi/util/ScrapingUtil.java
@@ -1,18 +1,46 @@
 package br.com.fra.brasileiraoapi.util;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.fra.brasileiraoapi.dto.PartidaGoogleDTO;
 
 public class ScrapingUtil {
 
-	private static final String BASE_URL_GOOGLE = "https://www.google.comr/search?q=";
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+	private static final String BASE_URL_GOOGLE = "https://www.google.com/search?q=";
 	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String partidaPesquisa = "palmeiras+x+coringhians+08/08/2020";
+		String url = BASE_URL_GOOGLE + partidaPesquisa + COMPLEMENTO_URL_GOOGLE;
 		
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
+	}
+	
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
 		PartidaGoogleDTO partida = new PartidaGoogleDTO();
-		partida.getGolsEquipeCasa();
-
+		
+		Document document = null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			
+			String title = document.title();
+			LOGGER.info("Titulo da Pagina {}", title);
+		} catch (IOException e) {
+			LOGGER.error("ERRO AO TENTAR CONECTAR NO GOOGLE -> {}", e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return partida;
+		
+		
 	}
 
 }
